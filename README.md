3d7f2ffd780c0bf9c330e52608dedafeb8cf7c23
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2019-11-15 14:23:48 -0800

    MVP Complete

--- README.md
+++ README.md
@@ -93,6 +93,7 @@ Before you get started, please take a few minutes and get acquainted with this a
 
 **STEP 4 - Consuming data with ProductContext**
 
+
 - Now that our `ProductContext` is now providing data we can finally consume it! To do so let's head over to our `Products` component and import the `useContext` hook as well as our `ProductContext`.
 
 - In the component, call the `useContext` hook and pass in the context object we want to use into it.
--- src/App.js
+++ src/App.js
@@ -6,35 +6,32 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		setCart([...cart, item]);
+		};
 		// add the given item to the cart
-	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{products, addItem}}>
+			<Route exact path="/" component={Products} />
+				<CartContext.Provider value={{ cart, setCart}}>
+					<Navigation />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+					{/* Routes */}
+					<Route 
+					path="/cart" 
+					render={() => <ShoppingCart cart={cart} />}
 					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
